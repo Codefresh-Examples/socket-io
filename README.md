ce85f8b02fbee6349a3fee8f08126e1a09afd495
Nathan Grunzweig <nathan@codefresh.io>
2015-01-14 16:30:26 +0200

    add socket-io example content

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# socket-io
-codefresh labs socket-io example
+# Socket Demonstration App
+
+A express node.js app built with Socket.io. For demonstration purposes and a tutorial.
+
+## Example
+
+The example show you a simple implementation of chat app using sockets
+
+### What to do?
+
+Start chatting
+
+## App run
+
+Click on the 'Play' button at the top toolbar.
+
+![Socketio-example](http://i.imgur.com/v4EczBp.png)
---
+++ app.js
@@ -0,0 +1,26 @@
+/*eslint-env node */
+ 
+var app = require('express')();
+var http = require('http').Server(app);
+// load socket server module
+var io = require('socket.io')(http);
+// set default server port
+var port = process.env.PORT || 9090;
+
+// set client index url (view)
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+// handle chat messages
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+// start the server
+http.listen(port, function(){
+    console.log('Express server listening on %d', port);
+});
